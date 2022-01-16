48f08bcdd8f7ba1be17c1aa1410f9a53885b6079
Jeong Chong In <chongin12@naver.com>
2022-01-16 04:44:31 +0000

    1146

--- acmicpc.net/1146.cpp
+++ acmicpc.net/1146.cpp
@@ -1,8 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long int;
-ll dp[101][101]; // dp[i][j] : 사람이 총 i명 있을 때 j번째 
+const ll P=1000000;
+ll dp[2][101][101];// d : 0이면 증가, 1이면 감소
+ll f(int d, int left, int right){
+	if(dp[d][left][right]!=-1) return dp[d][left][right];
+	if(left==0 && right==0) return dp[d][left][right]=1;
+	ll sum=0;
+	if(d==0){ // left에 있는 걸 선택해야 함.
+		for(int i=0; i<left; ++i){
+			sum+=f(1,i,left+right-i-1);
+			sum%=P;
+		}
+	}
+	else{
+		for(int i=0; i<right; ++i){
+			sum+=f(0,left+i,right-i-1);
+			sum%=P;
+		}
+	}
+	return dp[d][left][right]=sum%P;
+}
 int main(){
-	int N; cin>>N;
-	
+	ll N ;cin>>N;
+	if(N==1) {
+		cout << "1\n";
+		return 0;
+	}
+	for(int i=0; i<2; ++i){
+		for(int j=0; j<101; ++j){
+			for(int k=0; k<101; ++k){
+				dp[i][j][k]=-1;
+			}
+		}
+	}
+	ll res=0;
+	for(int i=1; i<=N; ++i){
+		for(int j=1; j<=N; ++j){
+			if(i==j) continue;
+			// cout << "i : " << i << ", j : " << j << " -> ";
+			if(i<j){
+				// cout << "d : "<<0<<", left : "<<j-2 <<", right : " << N-j << '\n';
+				res+=f(0,j-2,N-j);
+				// cout << "+="<<f(0,j-2,N-j) << '\n';
+			}
+			else{
+				// cout << "d : "<<1<<", left : "<<j-1 <<", right : " << N-j-1 << '\n';
+				res+=f(1,j-1,N-j-1);
+				// cout << "+="<<f(0,j-2,N-j) << '\n';
+			}
+			res%=P;
+		}
+	}
+	cout << res << '\n';
 }
---
+++ contest746/A.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long int;
+int main(){
+	ll N,R; cin>>N>>R;
+	cout << N-1+2*R << '\n';
+}
