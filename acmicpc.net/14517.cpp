564225f46d5c0ed248bb7cd8549dac20d6ba0331
Jeong Chong In <chongin12@naver.com>
2022-02-22 18:36:34 +0900

    14505, 14517

---
+++ acmicpc.net/14517.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[1001][1001];
+int main(){
+    string str; cin>>str;
+    int N=str.size();
+    for(int k=1; k<=N; ++k){//문자 k개를 본다.
+        for(int i=0; i<N-k+1; ++i){
+            int j=i+k-1;
+            //i ~ j 탐색
+            if(k==1) dp[i][j]=1;
+            else if(k==2){
+                if(str[i]==str[j]) dp[i][j]=3;
+                else dp[i][j]=2;
+            }
+            else{
+                dp[i][j]=-dp[i+1][j-1]+dp[i][j-1]+dp[i+1][j]+10007;
+                if(str[i]==str[j]){
+                    dp[i][j]+=dp[i+1][j-1]+1;
+                }
+            }
+            dp[i][j]%=10007;
+            // cout << "dp["<<i<<"]["<<j<<"]="<<dp[i][j]<<'\n';
+        }
+    }
+    cout << dp[0][N-1]%10007 << '\n';
+}
