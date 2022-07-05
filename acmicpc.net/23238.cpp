2df7e10c35b4e8821eea83ce15f3036a15d250db
Jeong Chong In <chongin12@naver.com>
2022-07-06 01:14:11 +0900

    2022.07.06

--- acmicpc.net/23238.cpp
+++ acmicpc.net/23238.cpp
@@ -1,129 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[100011];
+const int MN=100000;
+int sz;
+int N,Q;
+int cnt[MN+10];
+int cntcnt[400][MN+10]; // cntcnt[i][j] : i번째 buck에서 j번만큼 나온 수들의 개수
+int buck[400];
+int arr[MN+10];
+int ori[MN+10];
+int res[MN+10];
 struct query {
-    int l, r;
+    int s,e;
     int idx;
-    query(int l, int r, int idx):l(l),r(r),idx(idx){}
+    query(int s, int e, int idx):s(s),e(e),idx(idx){}
 };
 vector<query> queries;
-int qres[100011];
-set<int> cnt[100011];
-map<int,int> m;
-int n, q;
-int k;
-bool cmp(query a, query b){
-    if(a.l/k != b.l/k){
-        return a.r<b.r;
-    }
-    return a.l/k<b.l/k;
+int maxNum, groupNum;
+void cntsUp(int i) {
+    int idx=i/sz;
+    if(cnt[i]==buck[idx]) ++buck[idx];
+    cntcnt[idx][cnt[i]]--;
+    cnt[i]++;
+    cntcnt[idx][cnt[i]]++;
+}
+void cntsDown(int i) {
+    int idx=i/sz;
+    cntcnt[idx][cnt[i]]--;
+    cnt[i]--;
+    cntcnt[idx][cnt[i]]++;
+    if(cntcnt[idx][buck[idx]]==0) buck[idx]--;
 }
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    cin>>n>>q;
-    k=sqrt(n);
-    for(int i=1; i<=n; ++i){
-        cin>>arr[i];
-        m[arr[i]]=0;
+    cin>>N>>Q;
+    sz=sqrt(N);
+    vector<pair<int,int> > compression;
+    for(int i=0; i<N; ++i) {
+        int a; cin>>a;
+        compression.push_back({a,i});
     }
-    for(int i=0; i<q; ++i){
+    sort(compression.begin(), compression.end());
+    int K=0;
+    for(int i=0; i<compression.size(); ++i){
+        if(i>0 && compression[i].first != compression[i-1].first){
+            K++;
+        }
+        ori[K]=compression[i].first;
+        arr[compression[i].second]=K;
+    }
+    maxNum=K;
+    groupNum=maxNum/sz+1;
+    for(int i=0; i<Q; ++i){
         int a,b; cin>>a>>b;
-        queries.push_back(query(a,b,i));
+        queries.push_back(query(a-1,b-1,i));
+    }
+    sort(queries.begin(), queries.end(), [&](query a, query b){
+        if(a.s/sz == b.s/sz) return a.e<b.e;
+        return a.s/sz<b.s/sz;
+    });
+
+    int S=0, E=-1;
+    for(int i=0; i<queries.size(); ++i){
+        int toS=queries[i].s, toE=queries[i].e, toIdx=queries[i].idx;
+        while(E<toE){
+            ++E;
+            cntsUp(arr[E]);
+        }
+        while(S>toS){
+            --S;
+            cntsUp(arr[S]);
+        }
+        while(E>toE){
+            cntsDown(arr[E]);
+            --E;
+        }
+        while(S<toS){
+            cntsDown(arr[S]);
+            ++S;
+        }
+
+        int maxBuckIdx=0;
+        for(int j=1; j<=N/sz; ++j){
+            if(buck[maxBuckIdx]<=buck[j]) maxBuckIdx=j;
+        }
+        int r=sz*maxBuckIdx;
+        for(int j=0; j<sz && sz*maxBuckIdx+j<N; ++j){
+            if(cnt[r]<=cnt[sz*maxBuckIdx+j]) r=sz*maxBuckIdx+j;
+        }
+        res[toIdx]=ori[r];
     }
 
-    sort(queries.begin(), queries.end(), cmp);
-    // for(auto it:queries) cout << it.l << ", " << it.r << '\n';
-    int l=queries[0].l;
-    int r=queries[0].r;
-    int res=0; // cnt의 idx 위를 움직임.
-    int resnum=0;
-    for(int i=l; i<=r; ++i){
-        cnt[m[arr[i]]].erase(arr[i]);
-        m[arr[i]]++;
-        cnt[m[arr[i]]].insert(arr[i]);
-        if(m[arr[i]]>res) {
-            res=m[arr[i]];
-            resnum=arr[i];
-        }
-        else if(m[arr[i]]==res) {
-            resnum=max(resnum, arr[i]);
-        }
-    }
-    // for(int j=0; j<=3; ++j){
-    //     cout << "j : " << j << '\n';
-    //     for(auto it=cnt[j].begin(); it!=cnt[j].end(); ++it){
-    //         cout << *it << '\n';
-    //     }
-    // }
-    qres[queries[0].idx]=resnum;
-    for(int i=1; i<queries.size(); ++i){
-        int nl=queries[i].l;
-        int nr=queries[i].r;
-        int nidx=queries[i].idx;
-        while(nr>r){
-            ++r;
-            cnt[m[arr[r]]].erase(arr[r]);
-            m[arr[r]]++;
-            cnt[m[arr[r]]].insert(arr[r]);
-            if(m[arr[r]]>res) {
-                res=m[arr[r]];
-                resnum=arr[r];
-            }
-            else if(m[arr[r]]==res){
-                resnum=max(resnum, arr[r]);
-            }
-        }
-        while(nr<r){
-            cnt[m[arr[r]]].erase(arr[r]);
-            m[arr[r]]--;
-            cnt[m[arr[r]]].insert(arr[r]);
-            if(cnt[res].size()==0) {
-                res--;
-                if(res==0) continue;
-            }
-            resnum=0;
-            for(auto it=cnt[res].begin(); it!=cnt[res].end(); ++it){
-                resnum=max(resnum, *it);
-            }
-            --r;
-        }
-        while(nl<l){
-            --l;
-            cnt[m[arr[l]]].erase(arr[l]);
-            m[arr[l]]++;
-            cnt[m[arr[l]]].insert(arr[l]);
-            if(m[arr[l]]>res) {
-                res=m[arr[l]];
-                resnum=arr[l];
-            }
-            else if(m[arr[l]]==res){
-                resnum=max(resnum, arr[l]);
-            }
-        }
-        while(nl>l){
-            cnt[m[arr[l]]].erase(arr[l]);
-            m[arr[l]]--;
-            cnt[m[arr[l]]].insert(arr[l]);
-            if(cnt[res].size()==0) {
-                res--;
-                if(res==0) continue;
-            }
-            resnum=0;
-            for(auto it=cnt[res].begin(); it!=cnt[res].end(); ++it){
-                resnum=max(resnum, *it);
-            }
-            ++l;
-        }
-        qres[nidx]=resnum;
-        // for(int j=0; j<=3; ++j){
-        //     cout << "j : " << j << '\n';
-        //     for(auto it=cnt[j].begin(); it!=cnt[j].end(); ++it){
-        //         cout << *it << '\n';
-        //     }
-        // }
-        // cout << "qres["<<nidx<<"]="<<resnum<<'\n';
-    }
-    for(int i=0; i<q; ++i){
-        cout << qres[i] << '\n';
+
+    for(int i=0; i<Q; ++i){
+        cout << res[i] << '\n';
     }
 }
