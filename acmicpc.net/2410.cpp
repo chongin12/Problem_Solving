b97aa6c2fcc33a70d75d34ccdb57a2d3b0c75e89
정종인 <chongin12@naver.com>
2022-10-05 11:12:31 +0900

    2022.10.05

--- acmicpc.net/2410.cpp
+++ acmicpc.net/2410.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 const int MOD=1e9;
 int dp[1000001];
+int f(int n){
+    if(n==0) return 1;
+    if(dp[n]) return dp[n];
+    int ret=0;
+    for(int i=0; ; ++i){
+        int k=(1<<i);
+        if(k>n) break;
+        if((n-k)%k) break;
+        ret+=f((n-k)/k);
+        ret%=MOD;
+    }
+    return dp[n]=ret;
+}
 int main(){
     int N; cin>>N;
-    dp[0]=0;
-    dp[1]=1;
-    int k=1;
-    for(int i=2; i<=N; ++i){
-        if(i==k*2){
-            k*=2;
-            dp[i]+=1;
-        }
-        else{
-            dp[i]=dp[k]*dp[i-k];
-        }
-        dp[i]%=MOD;
-        cout << "dp["<<i<<"]="<<dp[i]<<'\n';
-    }
-    cout << dp[N] << '\n';
+    for(int i=0; i<N; ++i) f(i);
+    cout << f(N) << '\n';
 }
