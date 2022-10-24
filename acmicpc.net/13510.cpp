fd3c77bd5c7c18607f4e44f47b8904acab2a02bc
정종인 <chongin12@naver.com>
2022-10-24 14:50:10 +0900

    2022.10.24

---
+++ acmicpc.net/13510.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+const int MN = 100001;
+ll seg[MN*4+1];
+struct SegTree {
+    SegTree(){}
+public:
+    void SegUpdate(int idx, ll val){
+        Update(idx, val, 1,1,MN);
+    }
+    ll SegQuery(int L, int R){
+        return Query(L, R, 1, 1, MN);
+    }
+private:
+    ll Update(int idx, ll val, int n, int l, int r) {
+        if(r<idx || idx<l) return seg[n];
+        if(l==r) return seg[n] = val;
+        int mid = (l+r)/2;
+        return seg[n] = Update(idx, val, n*2, l, mid) + Update(idx, val, n*2+1, mid+1, r);
+    }
+    ll Query(int L, int R, int n, int l, int r) {
+        if (r<L || R<l) return 0;
+        if(L<=l && r<=R) return seg[n];
+        int mid = (l+r)/2;
+        return Query(L, R, n*2, l, mid) + Query(L, R, n*2+1, mid+1, r);
+    }
+};
+const int MAX_V = 100001;
+int Sz[MAX_V], Dep[MAX_V], Par[MAX_V], Top[MAX_V], In[MAX_V], Out[MAX_V];
+vector<int> G[MAX_V];
+struct HLD {
+    /*
+     * Sz[v] : v를 루트로 하는 서브트리의 크기
+     * Dep[v] : v의 깊이
+     * Par[v] : v의 부모 정점
+     * Top[v] : v가 속한 체인에서 맨 위에 있는 정점
+     * In[v] : DFS에서 v에 들어가는 시간
+     * Out[v] : DFS에서 v를 빠져 나오는 시간
+     * G[v] : v의 자식 정점 
+     */
+    SegTree segTree = SegTree();
+    HLD(int root){
+        cout << "asdf\n";
+        DFS1(root);
+        cout << "qwer\n";
+        DFS2(root);
+    }
+    void DFS1(int v){
+        Sz[v] = 1;
+        for(auto &i : G[v]){
+            Dep[i] = Dep[v] + 1; Par[i] = v;
+            DFS1(i);
+            Sz[v]+=Sz[i];
+            if(Sz[i] > Sz[G[v][0]]) swap(i, G[v][0]);
+        }
+    }
+    void DFS2(int v) {
+        static int cnt=0;
+        In[v] = ++cnt;
+        for(auto i : G[v]){
+            Top[i] = i == G[v][0] ? Top[v]:i;
+            DFS2(i);
+        }
+        Out[v] = cnt;
+    }
+    ll PathQuery(int u, int v){
+        ll ret=0;
+        for(; Top[u]!=Top[v]; u=Par[Top[u]]){
+            if(Dep[Top[u]] < Dep[Top[v]]) swap(u,v);
+            ret+=SegQuery(In[Top[u]], In[u]);
+        }
+        if(In[u] > In[v]) swap(u,v);
+        ret += SegQuery(In[u], In[v]);
+        return ret;
+    }
+    void Update(int idx, ll val){
+        segTree.SegUpdate(idx, val);
+    }
+private:
+    ll SegQuery(int a, int b){
+        return segTree.SegQuery(a,b);
+    }
+};
+vector<pair<int,int> > temp[100001];
+int edgeInfo[100001]; // i번째 간선이 가리키는 정점
+void dfs(int x, HLD &hld){
+    for(auto it:temp[x]){
+        int dst=it.first;
+        int w=it.second;
+        hld.Update(dst, w);
+        dfs(dst, hld);
+    }
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int N; cin>>N;
+    vector<pair<int, pair<int, int>> > edges;
+    for(int i=0; i<N-1; ++i){
+        int u, v, w; cin>>u>>v>>w;
+        edges.push_back({u,{v,w}});
+        temp[u].push_back({v,w});
+        temp[v].push_back({u,w});
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    HLD hld = HLD(1);
+    int cnt=0;
+    for(auto it:edges){
+        ++cnt;
+        int u=it.first;
+        int v=it.second.first;
+        int w=it.second.second;
+        if(Par[u]==v){
+            edgeInfo[cnt]=u;
+        }
+        else{
+            edgeInfo[cnt]=v;
+        }
+        hld.Update(edgeInfo[cnt], w);
+    }
+
+    int query; cin>>query;
+    while(query--){
+        int ch; cin>>ch;
+        if(ch==1){
+            int i, c; cin>>i>>c;
+            hld.Update(edgeInfo[i], c);
+        }
+        else{
+            int u, v; cin>>u>>v;
+            cout << hld.PathQuery(u, v) << '\n';
+        }
+    }
+}
