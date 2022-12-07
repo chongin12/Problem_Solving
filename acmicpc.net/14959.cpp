10f283a986089ca63420e8affac647ad87f3e281
Jeong Chong In <chongin12@naver.com>
2022-12-07 17:51:24 +0900

    2022.12.07

--- acmicpc.net/14959.cpp
+++ acmicpc.net/14959.cpp
@@ -2,29 +2,33 @@
 using namespace std;
 int S[1000001];
 int F[1000001];
+int tp,tk,now;
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int N; cin>>N;
     for(int i=0; i<N; ++i){
         cin>>S[N-i-1];
     }
+    int k=N-1,p=1;
     for(int i=1, j=0; i<N; ++i){
         while(j>0 && S[j]!=S[i]) j=F[j-1];
         if(S[j]==S[i]) F[i]=++j;
-        cout << "F["<<i<<"]="<<F[i]<<'\n';
+        
     }
-    int k=0,p=N;
-    for(int i=1, j=0; i<N; ++i){
-        // k와 p 찾기
-        while(j>0 && S[j]!=S[i]) j=F[j-1];
-        if(S[j]==S[i]) F[i]=++j;
-        int tk=N-i-1, tp=F[i];
-        cout << "tk : " << tk << ", tp : " << tp << '\n';
-        if(tp>0 && k+p>tk+tp){
-            k=tk;
-            p=tp;
+    for(int i=N-1; i>=0; --i){
+        tp = i+1-F[i];
+        tk = N-i-1;
+        if(!(tk+tp<k+p || (tk+tp==k+p && tp<p))) continue;
+        now=i-tp;
+        bool pass=true;
+        while(now+1>tp){
+            if(now+1-F[now]!=tp){
+                pass=false;
+                break;
+            }
+            now=now-tp;
         }
-        else if(tp>0 && k+p==tk+tp && p>tp){
+        if(pass){
             k=tk;
             p=tp;
         }
--- acmicpc.net/test.cpp
+++ acmicpc.net/test.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 using namespace std;
-int dp[1000];
-int arr[1000];
-int main(){
-    int N; cin >> N;
-    for(int i=0; i<N; ++i){
-        cin >> arr[i];
+
+class Stack {
+    int size;
+    int* mem; // 동적 메모리 할당. 스택 공간
+    int tos; // top of stack. -1이면 스택이 비었음
+public:
+    Stack(int size = 10) {
+        this->size = size;
+        mem = new int [size]; // int [10] 동적 할당.
+        tos = -1; // 스택이 비어있음
     }
+    ~Stack() { delete [] mem; } // 배열 반환
+    Stack& operator << (int n);
+    Stack& operator >> (int& n);
+    bool operator ! ();
+};
 
-    int res=-1;
-
-    for(int i=0; i<N; ++i){
-        dp[i] = max(dp[i], 1);
-
-        for(int j=i+1; j<N; ++j){
-            if(arr[i] < arr[j]) {
-                dp[i] = max(dp[j], dp[i]+1);
-            }
-        }
-        res = max(res, dp[i]);
+Stack& Stack::operator << (int n) { // 푸시
+    if(tos == size-1) {
+        return this;
     }
-    cout << res << '\n';
+    tos++; // 스택 탑을 증가
+    mem[tos] = n;
+    return this;
+} 
+
+Stack& Stack::operator >> (int& n) { // 팝
+    if(tos == -1) {
+        return this;
+    } 
+    n = mem[tos]; // top에 있는 스택 값 리턴
+    tos--; // 스택 top 감소
+    return this;
+} 
+
+bool Stack::operator ! () { // 비어 있으면 true 리턴
+    if(tos == -1)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    Stack stack;
+    stack << 3 << 5 << 10; // 3, 5, 10을 순서대로 푸시
+    while(true) {
+        if(!stack) break; // 스택 empty
+        int x;
+        stack >> x; // 스택의 탑에 있는 정수 팝
+        cout << x << ' ';
+    }
+    cout << endl;
 }
