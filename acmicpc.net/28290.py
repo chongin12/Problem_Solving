24a889f2ecf09b0c03b42628b5fa0d895c769db8
chongin12 <chongin12@naver.com>
2023-07-23 23:58:13 +0900

    2023.07.23

---
+++ acmicpc.net/28290.py
@@ -0,0 +1,6 @@
+a=input()
+if a=="fdsajkl;" or a=="jkl;fdsa":print("in-out")
+elif a=="asdf;lkj" or a==";lkjasdf":print("out-in")
+elif a=="asdfjkl;":print("stairs")
+elif a==";lkjfdsa":print("reverse")
+else:print("molu")
