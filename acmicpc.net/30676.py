5c9cb55ba98076a5eaff34bd5a7138c76d037214
Jeong Chong In <chongin12@naver.com>
2023-12-02 16:13:25 +0900

    2023.12.02

---
+++ acmicpc.net/30676.py
@@ -0,0 +1,10 @@
+n=int(input())
+s=''
+if n>=620:s="Red"
+elif n>=590:s="Orange"
+elif n>=570:s="Yellow"
+elif n>=495:s="Green"
+elif n>=450:s="Blue"
+elif n>=425:s="Indigo"
+elif n>=380:s="Violet"
+print(s)
