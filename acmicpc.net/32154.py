84d6bf1d0cd4563e0d79a76434ec1755531b789c
chongin12(Mosu) <chongin12@naver.com>
2024-10-19 14:49:32 +0900

    2024.10.19

---
+++ acmicpc.net/32154.py
@@ -0,0 +1,12 @@
+print([
+    "11\nA B C D E F G H J L M",
+    "9\nA C E F G H I L M",
+    "9\nA C E F G H I L M",
+    "9\nA B C E F G H L M",
+    "8\nA C E F G H L M",
+    "8\nA C E F G H L M",
+    "8\nA C E F G H L M",
+    "8\nA C E F G H L M",
+    "8\nA C E F G H L M",
+    "8\nA B C F G H L M"
+][int(input())-1])
