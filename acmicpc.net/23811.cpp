b1856d70cd0eb11a9d4758ab052bb8ae49981b7f
chongin12(Mosu) <chongin12@naver.com>
2025-01-10 18:41:16 +0900

    2025.01.10

---
+++ acmicpc.net/23811.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int N; cin>>N;
+    for(int k=0; k<N; ++k){
+        for(int i=0; i<5*N; ++i){
+            cout << "@";
+        }
+        cout << '\n';
+    }
+    for(int k=0; k<N; ++k){
+        for(int i=0; i<N; ++i){
+            cout << "@";
+        }
+        cout << '\n';
+    }
+    for(int k=0; k<N; ++k){
+        for(int i=0; i<5*N; ++i){
+            cout << "@";
+        }
+        cout << '\n';
+    }
+    for(int k=0; k<N; ++k){
+        for(int i=0; i<N; ++i){
+            cout << "@";
+        }
+        cout << '\n';
+    }
+    for(int k=0; k<N; ++k){
+        for(int i=0; i<5*N; ++i){
+            cout << "@";
+        }
+        cout << '\n';
+    }
+}
