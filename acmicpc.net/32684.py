d00949a58e96f68f0c598a16e5a65ce595e45dbe
chongin12(Mosu) <chongin12@naver.com>
2025-05-25 03:01:05 +0900

    2025.05.24

---
+++ acmicpc.net/32684.py
@@ -0,0 +1,6 @@
+a,b,c,d,e,f=map(int,input().split())
+r1=a*13+b*7+c*5+d*3+e*3+f*2
+a,b,c,d,e,f=map(int,input().split())
+r2=a*13+b*7+c*5+d*3+e*3+f*2+1.5
+if r1>r2:print("cocjr0208")
+else:print("ekwoo")
