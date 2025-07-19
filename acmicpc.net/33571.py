944f27083a4114d09404e08685380f0d6c1bab48
chongin12(Mosu) <chongin12@naver.com>
2025-07-19 15:53:42 +0900

    2025.07.19

---
+++ acmicpc.net/33571.py
@@ -0,0 +1,5 @@
+r=0
+for i in input():
+    if i=="B":r+=2
+    elif i=="A" or i=="a" or i=="b" or i=="D" or i=="e" or i=="d" or i=="g" or i=="O" or i=="o" or i=="P" or i=="p" or i=="Q" or i=="q" or i=="R" or i=="@":r+=1
+print(r)
