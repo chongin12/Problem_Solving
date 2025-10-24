f899c47ef60ceeea1e4ca92924a2e75691e6e71f
chongin12(Mosu) <chongin12@naver.com>
2025-10-24 22:18:46 +0900

    2025.10.24

---
+++ acmicpc.net/9372.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T; cin>>T;
+    while(T--){
+        int N, M; cin>>N>>M;
+        for(int i=0; i<M; ++i){
+            int u,v; cin>>u>>v;
+        }
+        cout << N-1 << '\n';
+    }
+}
