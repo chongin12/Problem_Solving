2ee8c38e47d8622f6e73c69ba2d8c8cd563f4db9
Jeong Chong In (Mosu) <chongin12@naver.com>
2026-03-05 23:28:19 +0900

    2026.03.05

---
+++ acmicpc.net/34691.py
@@ -0,0 +1,8 @@
+while True:
+    a=input()
+    s=""
+    if a=="end":break
+    elif a=="animal":s="Panthera tigris"
+    elif a=="tree":s="Pinus densiflora"
+    else:s="Forsythia koreana"
+    print(s)
